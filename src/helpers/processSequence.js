5d36ab4ac3fdb0797922d53097e8c29a79ea6e67
nevsky118 <nevsky118@gmail.com>
2024-07-23 22:43:47 +0300

    Update processSequence.js

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -1,3 +1,5 @@
+import * as R from 'ramda';
+
 /**
  * @file Домашка по FP ч. 2
  *
@@ -18,34 +20,88 @@
 
  const api = new Api();
 
- /**
-  * Я – пример, удали меня
-  */
- const wait = time => new Promise(resolve => {
-     setTimeout(resolve, time);
- })
+const isGreaterThanTwo = R.compose(R.gt(R.__, 2), R.length);
+const isLessThanTen = R.compose(R.lt(R.__, 10), R.length);
+const isLengthValid = R.both(isGreaterThanTwo, isLessThanTen);
+const isPositiveNumber = R.compose(R.gt(R.__, 0), parseFloat);
+const isDecimal = R.test(/^[0-9.]+$/);
+const getResult = R.prop('result');
+const round = R.compose(Math.round, parseFloat);
 
- const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-     /**
-      * Я – пример, удали меня
-      */
-     writeLog(value);
+const validate = R.allPass([isLengthValid, isPositiveNumber, isDecimal]);
 
-     api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-         writeLog(result);
-     });
+const toBinary = R.pipe(
+    value => api.get('https://api.tech/numbers/base', { from: 10, to: 2, number: value }),
+    R.andThen(getResult),
+    R.otherwise(error => {
+        throw new Error(error.message);
+    })
+);
 
-     wait(2500).then(() => {
-         writeLog('SecondLog')
+const toSquare = R.pipe(
+    R.flip(Math.pow)(2)
+)
 
-         return wait(1500);
-     }).then(() => {
-         writeLog('ThirdLog');
+const remainderOfThree = R.pipe(
+    R.modulo(R.__, 3)
+)
 
-         return wait(400);
-     }).then(() => {
-         handleSuccess('Done');
-     });
- }
+const getName = R.pipe(
+    value => api.get(`https://animals.tech/${value}`, {}),
+    R.andThen(getResult),
+    R.otherwise(error => {
+        throw new Error(error.message);
+    })
+);
+
+const createLogValue = writeLog => v => R.tap(writeLog, v);
+const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
+    const logValue = createLogValue(writeLog);
+
+    const validateAndReturn = R.ifElse(
+        validate,
+        R.identity,
+        R.compose(R.partial(handleError, ['ValidationError']), R.identity)
+    )
+
+    const toBinaryAndLog = R.pipe(
+        toBinary,
+        R.andThen(logValue)
+    )
+
+    const toLengthAndLog = R.pipe(
+        R.length,
+        logValue
+    )
+
+    const toSquareAndLog = R.pipe(
+        toSquare,
+        logValue
+    )
+
+    const remainderOfThreeAndLog = R.pipe(
+        remainderOfThree,
+        logValue
+    )
+
+    const getNameAndLog = R.pipe(
+        getName,
+        R.andThen(handleSuccess)
+    )
+
+    const f = R.pipe(
+        logValue,
+        validateAndReturn,
+        round,
+        logValue,
+        toBinaryAndLog,
+        R.andThen(toLengthAndLog),
+        R.andThen(toSquareAndLog),
+        R.andThen(remainderOfThreeAndLog),
+        R.andThen(getNameAndLog),
+    )
+
+    f(value);
+}
 
 export default processSequence;
